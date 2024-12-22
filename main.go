cad71f1e2c3d9cd88b7e3f99311afaa5e5fb4f9b
zaifeng <zaifeng2023@gmail.com>
2024-12-22 14:18:58 +0800

    version fixed

--- main.go
+++ main.go
@@ -3,8 +3,5 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, world")
-    fmt.Println("Hello, golang")
-
-    fmt.Println("Hello, freedom")
+    fmt.Println("Hello, Golang")
 }
